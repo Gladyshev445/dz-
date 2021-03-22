bbc02689fee57b61ee5d0f43c9381a7151bdc2f1
Gladyshev445 <74065918+Gladyshev445@users.noreply.github.com>
2021-03-22 11:35:42 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+// let x = prompt("Введите первое число")
+//     y = prompt("Введите второе число")
+// if (x == y){
+//     alert("Числа равняются 0")
+//     console.log(x - x)
+//     console.log(y - y)
+// }
+// else{
+//     alert(x + y)
+// }
+
+// let x = prompt("Введите первое число")
+//     y = prompt("Введите второе число")
+// if (x == y){
+//     alert("Числа равняются 0")
+//     console.log(x - x)
+//     console.log(y - y)
+// }
+// else{
+//     x++
+//     y++
+//     alert(x + " и " + y)
+// }
+
